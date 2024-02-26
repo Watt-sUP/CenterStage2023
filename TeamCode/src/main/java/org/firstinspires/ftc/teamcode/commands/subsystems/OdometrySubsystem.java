b6dec6c72ec6247fcfa10b94a245c52549a457d1
Moraru Cosmin <cosminmoraru468@gmail.com>
2024-02-26 15:50:59 +0200

    Updated OdometrySubsystem interpolation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/subsystems/OdometrySubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/subsystems/OdometrySubsystem.java
@@ -2,12 +2,15 @@ package org.firstinspires.ftc.teamcode.commands.subsystems;
 
 import android.util.Pair;
 
+import androidx.annotation.NonNull;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
 import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 
@@ -37,8 +40,10 @@ public class OdometrySubsystem {
         Wheels.RIGHT.setServo(right);
         Wheels.PERPENDICULAR.setServo(back);
 
-        Wheels.LEFT.createSpline(new Pair<>(0.0, 93.0), new Pair<>(1.0, 125.0));
-        Wheels.RIGHT.createSpline(new Pair<>(0.0, 87.0), new Pair<>(1.0, 115.0));
+        Wheels.LEFT.createSpline(new Pair<>(0.0, 110.0), new Pair<>(1.0, 128.0));
+        Wheels.RIGHT.createSpline(new Pair<>(0.0, 99.0), new Pair<>(1.0, 119.0));
+
+        // TODO: Find positions for this wheel
         Wheels.PERPENDICULAR.createSpline(new Pair<>(0.0, 400.0), new Pair<>(0.5, 270.0), new Pair<>(1.0, 180.0));
     }
 
@@ -64,6 +69,12 @@ public class OdometrySubsystem {
             this.servo = servo;
         }
 
+        /**
+         * <p>Creates an interpolator for position generation in a relevant range.</p>
+         * <p>Typical usage consists of restricting the servomotor to certain angles with a range from 0 to 1.</p>
+         *
+         * @param points A list of sample points for the generator to use (must use at least 2 pairs)
+         */
         @SafeVarargs
         public final void createSpline(Pair<Double, Double>... points) {
             double[] x = Stream.of(points)
@@ -73,17 +84,37 @@ public class OdometrySubsystem {
                     .mapToDouble(p -> p.second)
                     .toArray();
 
-            spline = new SplineInterpolator().interpolate(x, y);
+            if (x.length == 2)
+                spline = new LinearInterpolator().interpolate(x, y);
+            else spline = new SplineInterpolator().interpolate(x, y);
         }
 
+        /**
+         * Sets the position of the wheel's servomotor based on its interpolated function
+         *
+         * @param position The position to set the wheel to
+         * @throws RuntimeException         If the servo wasn't set or positions weren't generated
+         * @throws IllegalArgumentException If the wheel is out of the generated range
+         */
         public void setToPosition(double position) throws RuntimeException {
             if (servo == null)
-                throw new RuntimeException("The servomotor of the" + this + "odometry isn't initialized");
+                throw new RuntimeException(String.format("The servomotor of the %s odometry isn't initialized", this));
 
             if (spline == null)
-                throw new RuntimeException("The positions of the " + this + "odometry weren't generated.");
+                throw new RuntimeException(String.format("The positions of the %s odometry weren't generated.", this));
 
+            if (!spline.isValidPoint(position)) {
+                double[] knots = spline.getKnots();
+                throw new IllegalArgumentException(String.format("Unable to access position %2f. " +
+                        "Spline positions range from [%.2f, %.2f]", position, knots[0], knots[knots.length - 1]));
+            }
             servo.setPosition(spline.value(position));
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return this.name().toLowerCase();
         }
     }
 }
