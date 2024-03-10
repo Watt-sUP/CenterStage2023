a60ad02d34f873dabe69635fe73d013735e97c17
Powercube7 <cosminmoraru468@gmail.com>
2024-03-10 21:37:52 +0200

    Fixed a ton more errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UtilityCameraFrameCapture.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UtilityCameraFrameCapture.java
@@ -37,16 +37,18 @@ import android.util.Pair;
 import android.util.Size;
 
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.button.Trigger;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.util.Timing;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
-import org.firstinspires.ftc.teamcode.roadrunner.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.subsystems.CollectorSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.OdometrySubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 import org.firstinspires.ftc.vision.VisionPortal;
 
 import java.util.Locale;
@@ -71,62 +73,43 @@ import java.util.concurrent.TimeUnit;
 @TeleOp(name = "Utility: Camera Frame Capture", group = "Utility")
 //@Disabled
 public class UtilityCameraFrameCapture extends LinearOpMode {
-    /*
-     * EDIT THESE PARAMETERS AS NEEDED
-     */
-
     public static boolean RANDOMIZE_LIGHTING = false;
     public static String IMAGE_NAME = "CameraCalibrationFrame";
-
-    // Internal state
-    boolean lastX;
-    int frameCount;
-    long capReqTime;
+    private final Timing.Timer cooldown = new Timing.Timer(1, TimeUnit.SECONDS);
+    private int frameCount;
 
     @Override
     public void runOpMode() {
-        VisionPortal portal;
-
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        CollectorSubsystem intake = new CollectorSubsystem(hardwareMap);
-        OdometrySubsystem odometry = new OdometrySubsystem(this);
-
-        portal = new VisionPortal.Builder()
+        VisionPortal portal = new VisionPortal.Builder()
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
                 .setCameraResolution(new Size(640, 480))
                 .build();
 
+        CommandScheduler scheduler = CommandScheduler.getInstance();
+        DriveSubsystem chassis = new DriveSubsystem(hardwareMap);
+        GamepadEx gamepad = new GamepadEx(gamepad1);
+
+        chassis.setAxes(gamepad::getLeftY, gamepad::getLeftX, gamepad::getRightX);
+        scheduler.registerSubsystem(chassis);
+        cooldown.start();
+
+        gamepad.getGamepadButton(GamepadKeys.Button.X)
+                .and(new Trigger(cooldown::done))
+                .whenActive(() -> {
+                    portal.saveNextFrameRaw(String.format(Locale.US, IMAGE_NAME + "-%06d", frameCount++));
+                    cooldown.start();
+
+                    if (RANDOMIZE_LIGHTING)
+                        randomizeLighting(portal);
+                });
+
         while (!isStopRequested()) {
-            boolean x = gamepad1.square;
-
-            drive.setWeightedDrivePower(new Pose2d(
-                    -gamepad1.left_stick_y,
-                    -gamepad1.left_stick_x,
-                    -gamepad1.right_stick_x
-            ));
-
-            if (x && !lastX) {
-                portal.saveNextFrameRaw(String.format(Locale.US, IMAGE_NAME + "-%06d", frameCount++));
-                capReqTime = System.currentTimeMillis();
-
-                if (RANDOMIZE_LIGHTING)
-                    randomizeLighting(portal);
-            }
-
-            lastX = x;
+            scheduler.run();
 
             telemetry.addLine("######## Camera Capture Utility ########");
             telemetry.addLine(" > Press Square to capture a frame");
             telemetry.addData(" > Camera Status", portal.getCameraState());
             telemetry.addData("Photos Taken", frameCount);
-
-            if (capReqTime != 0) {
-                telemetry.addLine("\nCaptured Frame!");
-            }
-
-            if (capReqTime != 0 && System.currentTimeMillis() - capReqTime > 1000) {
-                capReqTime = 0;
-            }
 
             telemetry.update();
         }
